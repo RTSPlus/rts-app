c441b396cea7e528267581102c661a1f2307e08f
Noah Bresler <noahbresler2002@gmail.com>
2023-03-29 01:53:20 -0400

    Revamp search body UI

--- src/components/SearchView/SearchViewBody.tsx
+++ src/components/SearchView/SearchViewBody.tsx
@@ -1,45 +1,66 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import { useQuery } from "@tanstack/react-query";
 import { Fragment } from "react";
 import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
 
 import { googleAutocomplete } from "./googleAutocompleteAPI";
 import { colors } from "../../colors";
 import useDebounce from "../../utils/useDebounce";
 
-const SearchItem = () => {
-  return (
-    <TouchableOpacity style={styles.searchItem}>
-      {/* Placeholder icon */}
-      <View
-        style={{
-          height: 32,
-          width: 32,
-          backgroundColor: "red",
-          borderRadius: 16,
-        }}
+const SearchItemIcons = {
+  LOCATION: (
+    <View
+      style={{
+        height: 32,
+        width: 32,
+        backgroundColor: colors.ios.light.red.toString(),
+        borderRadius: 16,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Ionicons
+        name="ios-pin"
+        size={22}
+        color="white"
+        style={{ transform: [{ translateX: 1.25 }] }}
       />
-      <View style={styles.searchItemContent}>
-        <Text style={styles.searchItemTitle}>Title</Text>
-        <Text style={styles.searchItemDesc}>Description</Text>
-      </View>
-    </TouchableOpacity>
-  );
-};
+    </View>
+  ),
+  PLACEHOLDER: (
+    <View
+      style={{
+        height: 32,
+        width: 32,
+        backgroundColor: colors.ios.light.blue.toString(),
+        borderRadius: 16,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Ionicons
+        name="ios-pin"
+        size={22}
+        color="white"
+        style={{ transform: [{ translateX: 1.25 }] }}
+      />
+    </View>
+  ),
+} as const;
 
-const GoogleAutocompleteSearchItem = (props: { description: string }) => {
+const SearchItem = (props: {
+  title: string;
+  description: string;
+  icon: (typeof SearchItemIcons)[keyof typeof SearchItemIcons];
+}) => {
   return (
     <TouchableOpacity style={styles.searchItem}>
       {/* Placeholder icon */}
-      <Icon
-        name="map-marker"
-        size={35}
-        color="black"
-        style={{ alignItems: "center" }}
-      />
+      {props.icon}
       <View style={styles.searchItemContent}>
-        <Text>{props.description}</Text>
+        <Text style={styles.searchItemTitle}>{props.title}</Text>
+        <Text style={styles.searchItemDesc}>0.5mi · {props.description}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -58,24 +79,46 @@ export default function SearchViewBody(props: Props) {
     enabled: debouncedSearchQuery.length > 0,
   });
 
+  const results: { item: JSX.Element; key: string }[] = [
+    // Google autocomplete items
+    ...(googleAutocompleteResults ?? []).map((item) => ({
+      key: item.place_id,
+      item: (
+        <SearchItem
+          title={item.terms[0].value}
+          description={`${item.terms[1]?.value ?? "empty"}, ${
+            item.terms[2]?.value ?? "empty"
+          }`}
+          icon={SearchItemIcons.LOCATION}
+        />
+      ),
+    })),
+
+    // Placeholder items
+    ...Array(15)
+      .fill(0)
+      .map((item, index) => ({
+        key: index.toString(),
+        item: (
+          <SearchItem
+            title="Title"
+            description="description"
+            icon={SearchItemIcons.PLACEHOLDER}
+          />
+        ),
+      })),
+  ];
+
   return (
     <>
       <View style={styles.topDivider} />
       <BottomSheetScrollView contentContainerStyle={styles.scrollView}>
-        {googleAutocompleteResults?.map((item, index) => (
-          <GoogleAutocompleteSearchItem
-            key={index}
-            description={item.description}
-          />
+        {results.map((item) => (
+          <Fragment key={item.key}>
+            {item.item}
+            <View style={styles.searchItemDivider} />
+          </Fragment>
         ))}
-        {Array(15)
-          .fill(0)
-          .map((item, index) => (
-            <Fragment key={index}>
-              <SearchItem key={index} />
-              <View style={styles.searchItemDivider} />
-            </Fragment>
-          ))}
       </BottomSheetScrollView>
     </>
   );
@@ -94,7 +137,7 @@ const styles = StyleSheet.create({
   searchItemDivider: {
     width: "100%",
     height: 1,
-    backgroundColor: colors.ios.light.gray["5"].toRgbString(),
+    backgroundColor: colors.ios.light.gray["4"].toRgbString(),
   },
   searchItem: {
     width: "100%",
@@ -108,10 +151,11 @@ const styles = StyleSheet.create({
   },
   searchItemTitle: {
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: "400",
   },
   searchItemDesc: {
     fontSize: 14,
     color: colors.ios.light.gray["1"].toRgbString(),
+    marginTop: 3,
   },
 });
