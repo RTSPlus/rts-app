c5ad4340d952054fdfdfb305cb0c773a957b7d7d
Noah Bresler <noahbresler2002@gmail.com>
2023-03-28 21:11:46 -0400

    Create a useDebounce hook

---
+++ src/utils/useDebounce.tsx
@@ -0,0 +1,16 @@
+// Hook
+
+import { useEffect, useState } from "react";
+
+// T is a generic type for value parameter, our case this will be string
+export default function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
