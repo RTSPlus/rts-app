6150ecc80e4a99d603ba0f13f2febddc875c9ff8
Noah Bresler <noahbresler2002@gmail.com>
2023-03-31 02:52:50 -0400

    Populate nearby section in HomeViewBody

--- src/components/MainSheet/HomeView/HomeViewBody.tsx
+++ src/components/MainSheet/HomeView/HomeViewBody.tsx
@@ -1,79 +1,121 @@
-import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
-import { Box, HStack } from "native-base";
-import { Text, TouchableOpacity, View } from "react-native";
+import {
+  BottomSheetScrollView,
+  useBottomSheetInternal,
+} from "@gorhom/bottom-sheet";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Animated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useDerivedValue,
+} from "react-native-reanimated";
 
-import CustomRouteIcon from "../../CustomRouteIcon";
+import { colors } from "../../../colors";
+import { dispatch } from "../../modals/ModalController";
 
-export default function HomeViewBody() {
+function RouteCircleItem() {
   return (
-    <BottomSheetScrollView>
-      {/* TODO: Temporary spacer */}
-      <View style={{ flex: 1, height: 20 }} />
-      {/* Routes near you header and all routes button */}
-      <HStack width="90%" space="45%">
-        <Text
+    <>
+      <TouchableOpacity
+        style={{
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        onPress={() => {
+          dispatch({ event: "OPEN_ROUTE", payload: { routeNumber: 21 } });
+        }}
+      >
+        <View
           style={{
-            justifyContent: "flex-start",
-            alignItems: "flex-start",
+            width: 56,
+            height: 56,
+            backgroundColor: colors.ios.light.red.toRgbString(),
+            borderRadius: 28,
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          🚌 routes near you
-        </Text>
-        <TouchableOpacity>
           <Text
             style={{
-              justifyContent: "flex-end",
-              alignItems: "flex-end",
-              color: "grey",
+              color: "white",
+              fontSize: 24,
+              fontWeight: "700",
             }}
           >
-            all routes
+            21
           </Text>
-        </TouchableOpacity>
-      </HStack>
-
-      <View>
-        {/* Routes Information Container */}
-        <Box
-          width="90%"
-          justifyContent="center"
-          alignItems="center"
-          bg={{
-            linearGradient: {
-              colors: ["#bdc3c7", "#2c3e50"],
-              start: [0, 0],
-              end: [1, 0],
-            },
-          }}
-          bgColor="red"
-          p="12"
-          rounded="xl"
-          _text={{
-            fontSize: "md",
-            fontWeight: "medium",
-            color: "warmGray.50",
-            textAlign: "center",
+        </View>
+        <Text style={{ marginTop: 6, fontSize: 15 }}>Route 21</Text>
+        <Text
+          style={{
+            fontSize: 13,
+            color: colors.ios.light.gray["2"].toRgbString(),
           }}
         >
-          <HStack space={2} alignItems="center">
-            <TouchableOpacity>
-              <CustomRouteIcon color="#539AA9" text="80" />
-            </TouchableOpacity>
+          5 mins
+        </Text>
+      </TouchableOpacity>
+    </>
+  );
+}
 
-            <TouchableOpacity>
-              <CustomRouteIcon color="#A9C5CA" text="9" />
-            </TouchableOpacity>
+export default function HomeViewBody() {
+  const { animatedIndex } = useBottomSheetInternal();
 
-            <TouchableOpacity>
-              <CustomRouteIcon color="#D5C9CA" text="22" />
-            </TouchableOpacity>
+  const bodyOpacity = useDerivedValue(
+    () =>
+      interpolate(animatedIndex.value, [0, 0.2], [0, 1], {
+        extrapolateRight: Extrapolation.CLAMP,
+      }),
+    []
+  );
+  const bodyAnimatedStyle = useAnimatedStyle(() => ({
+    opacity: bodyOpacity.value,
+  }));
 
-            <TouchableOpacity>
-              <CustomRouteIcon color="blue" text="30" />
-            </TouchableOpacity>
-          </HStack>
-        </Box>
-      </View>
+  return (
+    <BottomSheetScrollView>
+      <Animated.View style={bodyAnimatedStyle}>
+        {/* TODO: Temporary spacer */}
+        <View style={{ flex: 1, height: 20 }} />
+
+        {/* Routes Near You */}
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Nearby</Text>
+          <View style={styles.sectionBody}>
+            <RouteCircleItem />
+            <View style={{ width: 30 }} />
+            <RouteCircleItem />
+            <View style={{ width: 20 }} />
+            <RouteCircleItem />
+          </View>
+        </View>
+      </Animated.View>
     </BottomSheetScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginHorizontal: 16,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "rgba(0,0,0,0.6)",
+  },
+
+  sectionBody: {
+    marginTop: 12,
+
+    height: 140,
+    borderRadius: 10,
+    backgroundColor: "white",
+
+    alignItems: "center",
+    flexDirection: "row",
+    // Remove
+    paddingLeft: 18,
+    gap: 20,
+  },
+});
