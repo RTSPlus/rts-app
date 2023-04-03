3f099879851e9e87c2bb7e27f1e94f119d7a3fe0
Noah Bresler <noahbresler2002@gmail.com>
2023-04-02 23:13:18 -0400

    State machine haptic feedback per row

--- src/components/modals/MapOptionsModal/RouteRow.tsx
+++ src/components/modals/MapOptionsModal/RouteRow.tsx
@@ -1,22 +1,98 @@
+import { useMachine } from "@xstate/react";
+import * as Haptics from "expo-haptics";
 import { useRef } from "react";
 import { View, Text, StyleSheet, Animated } from "react-native";
 import { RectButton, Swipeable } from "react-native-gesture-handler";
+import { createMachine } from "xstate";
 
 import { colors } from "../../../colors";
 import { RouteData } from "../../../rts-api/getRoutes";
+import { match } from "ts-pattern";
+
+type HapticFeedbackMachineEvent =
+  | { type: "PASS_ACTIVE_THRESHOLD" }
+  | { type: "BACK_TO_INACTIVE_THRESHOLD" }
+  | { type: "CLOSE" }
+  | { type: "RESET" };
+
+const hapticFeedbackMachine = createMachine({
+  tsTypes: {} as import("./RouteRow.typegen").Typegen0,
+  schema: {
+    events: {} as HapticFeedbackMachineEvent,
+  },
+  id: `hapticFeedbackMachine`,
+  initial: "idle",
+  states: {
+    idle: {
+      on: {
+        PASS_ACTIVE_THRESHOLD: {
+          target: "active",
+          actions: [
+            () => console.log("pass active threshold"),
+            () => {
+              Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+            },
+          ],
+        },
+      },
+    },
+    active: {
+      on: {
+        BACK_TO_INACTIVE_THRESHOLD: {
+          target: "idle",
+          actions: [
+            () => console.log("pass inactive threshold"),
+            () => {
+              Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+            },
+          ],
+        },
+        CLOSE: {
+          target: "closed",
+        },
+      },
+    },
+    closed: {
+      on: {
+        RESET: {
+          target: "idle",
+        },
+      },
+    },
+  },
+  predictableActionArguments: true,
+});
 
 export default function RouteRow(props: { routeItem: RouteData }) {
-  const swipeableRowLeftRef = useRef<Swipeable>(null);
-  const swipeableRowRightRef = useRef<Swipeable>(null);
+  const swipeableRowRef = useRef<Swipeable>(null);
+
+  const [hapticFeedbackLeftState, hapticFeedbackLeftSend] = useMachine(
+    () => hapticFeedbackMachine
+  );
+  const [hapticFeedbackRightState, hapticFeedbackRightSend] = useMachine(
+    () => hapticFeedbackMachine
+  );
 
   const renderLeftActions = (_progress: number, dragX: Animated.Value) => {
+    dragX.addListener(({ value }) => {
+      if (hapticFeedbackLeftState.matches("idle")) {
+        if (value >= 150) {
+          hapticFeedbackLeftSend("PASS_ACTIVE_THRESHOLD");
+        }
+      } else if (hapticFeedbackLeftState.matches("active")) {
+        if (value < 150) {
+          hapticFeedbackLeftSend("BACK_TO_INACTIVE_THRESHOLD");
+        }
+      }
+    });
+
     const trans = dragX.interpolate({
       inputRange: [0, 50, 100, 101],
       outputRange: [-20, 0, 0, 1],
     });
 
     const closeSwipeableRow = () => {
-      swipeableRowLeftRef?.current?.close();
+      swipeableRowRef?.current?.close();
     };
 
     return (
@@ -39,14 +115,25 @@ export default function RouteRow(props: { routeItem: RouteData }) {
     _progress: Animated.Value,
     dragX: Animated.Value
   ) => {
+    dragX.addListener(({ value }) => {
+      if (hapticFeedbackRightState.matches("idle")) {
+        if (value <= -150) {
+          hapticFeedbackRightSend("PASS_ACTIVE_THRESHOLD");
+        }
+      } else if (hapticFeedbackRightState.matches("active")) {
+        if (value > -150) {
+          hapticFeedbackRightSend("BACK_TO_INACTIVE_THRESHOLD");
+        }
+      }
+    });
+
     const trans = dragX.interpolate({
-      inputRange: [-100, 0],
-      outputRange: [0, 64],
-      extrapolate: "clamp",
+      inputRange: [-101, -100, 0],
+      outputRange: [0, 0, 64],
     });
 
     const closeSwipeableRow = () => {
-      swipeableRowRightRef?.current?.close();
+      swipeableRowRef?.current?.close();
     };
 
     return (
@@ -62,9 +149,31 @@ export default function RouteRow(props: { routeItem: RouteData }) {
 
   return (
     <Swipeable
-      ref={swipeableRowLeftRef}
+      ref={swipeableRowRef}
       renderLeftActions={renderLeftActions}
       renderRightActions={renderRightActions}
+      onSwipeableWillClose={(direction) => {
+        match(direction)
+          .with("left", () => hapticFeedbackLeftSend("CLOSE"))
+          .with("right", () => hapticFeedbackRightSend("CLOSE"))
+          .exhaustive();
+      }}
+      onSwipeableClose={(direction) => {
+        match(direction)
+          .with("left", () => hapticFeedbackLeftSend("RESET"))
+          .with("right", () => hapticFeedbackRightSend("RESET"))
+          .exhaustive();
+      }}
+      rightThreshold={Number.POSITIVE_INFINITY}
+      leftThreshold={Number.POSITIVE_INFINITY}
+      onCancelled={() => {
+        hapticFeedbackLeftSend("CLOSE");
+        hapticFeedbackRightSend("CLOSE");
+      }}
+      onEnded={() => {
+        hapticFeedbackLeftSend("CLOSE");
+        hapticFeedbackRightSend("CLOSE");
+      }}
     >
       <View style={rowStyles.row}>
         <View
