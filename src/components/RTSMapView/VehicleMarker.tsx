7e5edef6d6cb22db246ac90bbe8158441ee58d25
Noah Bresler <noahbresler2002@gmail.com>
2023-04-03 20:58:13 -0400

    Add marker svg

--- src/components/RTSMapView/VehicleMarker.tsx
+++ src/components/RTSMapView/VehicleMarker.tsx
@@ -8,6 +8,8 @@ import Animated, {
 
 import { projectPdistToPathPoint } from "../../rts-api/rts";
 import { PathPoint } from "../../rts-api/types";
+import { View, Text } from "react-native";
+import { G, Svg, Path } from "react-native-svg";
 
 export type VehicleMarkerProps = { path: PathPoint[] } & Omit<
   MapMarkerProps,
@@ -41,18 +43,56 @@ const VehicleMarker = forwardRef<VehicleMarkerRef, VehicleMarkerProps>(
     useAnimatedReaction(
       () => pdist.value,
       (newPdist) => {
-        // const newCoords = projectPdistToPathPoint(props.path, newPdist);
-        // internalMarkerRef.current?.setNativeProps({
-        //   coordinate: {
-        //     latitude: newCoords.lat,
-        //     longitude: newCoords.lon,
-        //   },
-        // });
+        const newCoords = projectPdistToPathPoint(props.path, newPdist);
+        internalMarkerRef.current?.setNativeProps({
+          coordinate: {
+            latitude: newCoords.lat,
+            longitude: newCoords.lon,
+          },
+        });
       },
       [pdist.value]
     );
 
-    return <CustomAnimatedMarker ref={internalMarkerRef} {...props} />;
+    return (
+      <CustomAnimatedMarker ref={internalMarkerRef} {...props}>
+        <View>
+          <Svg viewBox="0 0 342 1016" width={342 / 10} height={1016 / 10}>
+            <G id="Layer">
+              <G id="Folder 1">
+                <Path
+                  fill="#000"
+                  fill-rule="evenodd"
+                  d="m341.6 169.4c0 175.4-170.6 338.4-170.6 338.4 0 0-170.6-163-170.6-338.4 0-93.4 76.4-169.2 170.6-169.2 94.2 0 170.6 75.8 170.6 169.2zm-33.8-2.9c0-75.5-61.3-136.8-136.8-136.8-75.5 0-136.8 61.3-136.8 136.8 0 75.5 61.3 136.8 136.8 136.8 75.5 0 136.8-61.3 136.8-136.8z"
+                />
+                <Path
+                  fill="#000"
+                  d="m109.8 252.5c0 7.3 5.9 13.3 13.2 13.3 7.4 0 13.3-6 13.3-13.3v-17h-26.5v17z"
+                />
+                <Path
+                  fill="#000"
+                  d="m206.4 252.5c0 7.3 5.9 13.3 13.2 13.3 7.5 0 13.5-6 13.5-13.3v-17h-26.8z"
+                />
+                <Path
+                  fill="#000"
+                  fill-rule="evenodd"
+                  d="m269.7 126.8c0 4.8-3.9 8.6-8.6 8.6h-11v100.1h-157.3v-143c0-9.4 7.6-17.1 17-17.1h123.2c9.4 0 17.1 7.7 17.1 17.1v25.6h11c4.7 0 8.6 3.9 8.6 8.7zm-129.3-29.5c0 3.6 2.9 6.5 6.5 6.5h48.8c3.6 0 6.5-2.9 6.5-6.5 0-3.6-2.9-6.5-6.5-6.5h-48.8c-3.6 0-6.5 2.9-6.5 6.5zm-8.6 115.1c0-4.9-3.9-8.7-8.8-8.7-4.8 0-8.7 3.8-8.7 8.7 0 4.8 3.9 8.7 8.7 8.7 4.9 0 8.8-3.9 8.8-8.7zm96.6 0c0-4.9-3.9-8.7-8.7-8.7-4.8 0-8.7 3.8-8.7 8.7 0 4.8 3.9 8.7 8.7 8.7 4.8 0 8.7-3.9 8.7-8.7zm8.3-94.3h-130.5v74.7h130.5z"
+                />
+                <Path
+                  fill="#000"
+                  d="m72.3 126.8c0 4.8 3.8 8.6 8.6 8.6h11v-17.3h-11c-4.8 0-8.6 3.9-8.6 8.7z"
+                />
+                <Path fill="#000" d="m181.4 130.1l3.1 3.1-39.5 39.5-3.1-3.1z" />
+                <Path
+                  fill="#000"
+                  d="m192.6 154.3l-26.5 26.5-3.1-3.1 26.5-26.5z"
+                />
+              </G>
+            </G>
+          </Svg>
+        </View>
+      </CustomAnimatedMarker>
+    );
   }
 );
 VehicleMarker.displayName = "VehicleMarker";
