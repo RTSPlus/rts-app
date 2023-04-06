bce6766eba701a5298a670e1ed33f20bb396f03c
Noah Bresler <noahbresler2002@gmail.com>
2023-04-05 20:52:41 -0400

    Add more short names

--- src/rts-api/routesMeta.ts
+++ src/rts-api/routesMeta.ts
@@ -1,4 +1,6 @@
 export const routesShortName = new Map<number, string>([
   [20, "Reitz → Oaks Mall"],
   [21, "Reitz → Cabana"],
+  [118, "Hub → Cultural Plaza"],
+  [119, "Hub → Family Housing"],
 ]);
