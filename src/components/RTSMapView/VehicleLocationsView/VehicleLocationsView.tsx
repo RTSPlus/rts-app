56eb0fa7bba95480822ff3164644a1857d32dfac
Noah Bresler <noahbresler2002@gmail.com>
2023-04-11 19:14:23 -0400

    Add color to VehicleMarker

--- src/components/RTSMapView/VehicleLocationsView/VehicleLocationsView.tsx
+++ src/components/RTSMapView/VehicleLocationsView/VehicleLocationsView.tsx
@@ -1,13 +1,12 @@
 // #region Imports
 import { useQueries } from "@tanstack/react-query";
 import { useEffect, useRef, useState } from "react";
-import { Polyline } from "react-native-maps";
 import { hasPresentKey } from "ts-is-present";
 
 import RoutesDisplay from "./RoutesDisplay";
 import { useAvailableRoutes } from "../../../hooks/useRoutes";
 import { getRoutePattern } from "../../../rts-api/rts";
-import { Pattern } from "../../../rts-api/types";
+import { Pattern, Route } from "../../../rts-api/types";
 import { feetToMeters } from "../../../utils/utils";
 import VehicleMarker, { VehicleMarkerRef } from "../VehicleMarker";
 import { useVehicleLocations } from "../useVehicleLocations";
@@ -45,16 +44,18 @@ export default function VehicleLocationsView({ selectedRoutes }: Props) {
     .filter(hasPresentKey("data"))
     .map((e) => e.data);
 
-  // routePatternQueries` -> `pidToPatternsMap` sync
+  // #region routePatternQueries -> pidToPatternsMap -> patternsToRouteMap sync
   // Following chunk is a bit icky
-  // We sync between `routePatternQueries` and `pidToPatternsMap`
-  // Listen for changes in `routePatternQueries`
-  // and update `routePatternsMap` accordingly so we don't need to keep redeclaring Map()
+  // We sync between `routePatternQueries` and `pidToPatternsMap` and patternsToRouteMap
+  // Listen for changes in `routePatternQueries` and update maps accordingly so we don't need to keep redeclaring Map()
   // Potentionally just a micro-optimization but stable references are good too
 
-  // routePatternsMap should be stable and not be wiped on re-renders unless necessary
-  // in the case that it is, change this to a ref
-  const [pidToPatternsMap] = useState(new Map<number, Pattern>());
+  const pidToPatternsMap = useRef<Map<number, Pattern>>(
+    new Map<number, Pattern>()
+  );
+  const patternsToRouteMap = useRef<Map<number, Route>>(
+    new Map<number, Route>()
+  );
 
   // useEffect not entirely necessary. Same logic will be run on every render anyways
   // because useQueries returns an unstable refernece
@@ -62,66 +63,67 @@ export default function VehicleLocationsView({ selectedRoutes }: Props) {
   useEffect(() => {
     routes.map((route) => {
       Object.values(route.patterns).forEach((pattern) => {
-        if (!pidToPatternsMap.has(pattern.id)) {
-          pidToPatternsMap.set(pattern.id, pattern);
-        }
+        pidToPatternsMap.current.set(pattern.id, pattern);
+        patternsToRouteMap.current.set(pattern.id, route);
       });
     });
-  }, [pidToPatternsMap, routePatternQueries, routes]);
+  }, [routePatternQueries, routes]);
+  // #endregion
 
+  // ------------------------------------------- TODO: abstract this out -------------------------------------------
   const vehicleLocations = useVehicleLocations(availableSelectedRoutes);
   const usedPathIDs = vehicleLocations.flatMap((e) =>
     (e.data ?? []).map((v) => v.pid)
   );
 
   // Vehicle animation handling
-  // useEffect(() => {
-  //   // Filter out queries that don't have data. Merge data with last updated time
-  //   const vehicleLocationsData = vehicleLocations.flatMap((query) =>
-  //     (query.data ?? []).map((e) => ({
-  //       ...e,
-  //       lastUpdatedAt: query.dataUpdatedAt,
-  //     }))
-  //   );
+  useEffect(() => {
+    // Filter out queries that don't have data. Merge data with last updated time
+    const vehicleLocationsData = vehicleLocations.flatMap((query) =>
+      (query.data ?? []).map((e) => ({
+        ...e,
+        lastUpdatedAt: query.dataUpdatedAt,
+      }))
+    );
 
-  //   // Vehicle animation handled here
-  //   // Check if vehicle location has been updated
-  //   vehicleLocationsData.forEach((vehicle) => {
-  //     // check last updated map and add if not present
-  //     const lastUpdatedMap = vehicleLocationLastUpdated.current;
-  //     if (!lastUpdatedMap.has(vehicle.vid)) {
-  //       lastUpdatedMap.set(vehicle.vid, {
-  //         lastUpdatedAt: vehicle.lastUpdatedAt,
-  //         pdist: vehicle.pdist,
-  //       });
-  //     }
+    // Vehicle animation handled here
+    // Check if vehicle location has been updated
+    vehicleLocationsData.forEach((vehicle) => {
+      // check last updated map and add if not present
+      const lastUpdatedMap = vehicleLocationLastUpdated.current;
+      if (!lastUpdatedMap.has(vehicle.vid)) {
+        lastUpdatedMap.set(vehicle.vid, {
+          lastUpdatedAt: vehicle.lastUpdatedAt,
+          pdist: vehicle.pdist,
+        });
+      }
 
-  //     const vehicleLastUpdate = lastUpdatedMap.get(vehicle.vid);
-  //     if (
-  //       vehicleLastUpdate !== undefined &&
-  //       vehicleLastUpdate.pdist !== vehicle.pdist
-  //     ) {
-  //       const path = pidToPatternsMap.get(vehicle.pid)?.path;
-  //       if (path === undefined) {
-  //         throw new Error(`Path for pid ${vehicle.pid} not found`);
-  //       }
+      const vehicleLastUpdate = lastUpdatedMap.get(vehicle.vid);
+      if (
+        vehicleLastUpdate !== undefined &&
+        vehicleLastUpdate.pdist !== vehicle.pdist
+      ) {
+        const path = pidToPatternsMap.current.get(vehicle.pid)?.path;
+        if (path === undefined) {
+          throw new Error(`Path for pid ${vehicle.pid} not found`);
+        }
 
-  //       // Animate marker
-  //       vehicleMarkerRefMap.current
-  //         .get(vehicle.vid)
-  //         ?.animatedPdist(
-  //           feetToMeters(vehicle.pdist),
-  //           vehicle.lastUpdatedAt - vehicleLastUpdate.lastUpdatedAt
-  //         );
+        // Animate marker
+        vehicleMarkerRefMap.current
+          .get(vehicle.vid)
+          ?.animatedPdist(
+            feetToMeters(vehicle.pdist),
+            vehicle.lastUpdatedAt - vehicleLastUpdate.lastUpdatedAt
+          );
 
-  //       // update last updated time and values
-  //       lastUpdatedMap.set(vehicle.vid, {
-  //         lastUpdatedAt: vehicle.lastUpdatedAt,
-  //         pdist: vehicle.pdist,
-  //       });
-  //     }
-  //   });
-  // }, [pidToPatternsMap, vehicleLocations]);
+        // update last updated time and values
+        lastUpdatedMap.set(vehicle.vid, {
+          lastUpdatedAt: vehicle.lastUpdatedAt,
+          pdist: vehicle.pdist,
+        });
+      }
+    });
+  }, [pidToPatternsMap, vehicleLocations]);
 
   return (
     <>
@@ -129,11 +131,12 @@ export default function VehicleLocationsView({ selectedRoutes }: Props) {
       <RoutesDisplay routes={routes} usedPathIDs={usedPathIDs} />
 
       {/* Draw vehicle locations */}
-      {/* {vehicleLocations
+      {vehicleLocations
         .flatMap((e) => e.data ?? [])
-        .filter((vehicle) => pidToPatternsMap.get(vehicle.pid)?.path)
+        .filter((vehicle) => pidToPatternsMap.current.get(vehicle.pid)?.path)
         .map((vehicle) => (
           <VehicleMarker
+            color={patternsToRouteMap.current.get(vehicle.pid)?.color ?? "#000"}
             key={vehicle.vid}
             ref={(ref) => {
               if (
@@ -146,9 +149,9 @@ export default function VehicleLocationsView({ selectedRoutes }: Props) {
             }}
             title={vehicle.vid.toString()}
             description={vehicle.des}
-            path={pidToPatternsMap.get(vehicle.pid)?.path ?? []}
+            path={pidToPatternsMap.current.get(vehicle.pid)?.path ?? []}
           />
-        ))} */}
+        ))}
     </>
   );
 }
--- src/components/RTSMapView/VehicleMarker.tsx
+++ src/components/RTSMapView/VehicleMarker.tsx
@@ -1,17 +1,17 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { MapMarkerProps, Marker } from "react-native-maps";
 import Animated, {
   useAnimatedReaction,
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
-import { G, Svg, Path } from "react-native-svg";
+import { Svg, Path, Circle } from "react-native-svg";
 
 import { projectPdistToPathPoint } from "../../rts-api/rts";
 import { PathPoint } from "../../rts-api/types";
 
-export type VehicleMarkerProps = { path: PathPoint[] } & Omit<
+export type VehicleMarkerProps = { path: PathPoint[]; color?: string } & Omit<
   MapMarkerProps,
   "coordinate"
 >;
@@ -37,7 +37,7 @@ const VehicleMarker = forwardRef<VehicleMarkerRef, VehicleMarkerProps>(
           pdist.value = withTiming(newPdist, { duration });
         },
       }),
-      []
+      [pdist]
     );
 
     useAnimatedReaction(
@@ -54,41 +54,50 @@ const VehicleMarker = forwardRef<VehicleMarkerRef, VehicleMarkerProps>(
       [pdist.value]
     );
 
+    const fillColor = props.color ?? "#000";
     return (
       <CustomAnimatedMarker ref={internalMarkerRef} {...props}>
         <View>
           <Svg viewBox="0 0 342 1016" width={342 / 10} height={1016 / 10}>
-            <G id="Layer">
-              <G id="Folder 1">
-                <Path
-                  fill="#000"
-                  fill-rule="evenodd"
-                  d="m341.6 169.4c0 175.4-170.6 338.4-170.6 338.4 0 0-170.6-163-170.6-338.4 0-93.4 76.4-169.2 170.6-169.2 94.2 0 170.6 75.8 170.6 169.2zm-33.8-2.9c0-75.5-61.3-136.8-136.8-136.8-75.5 0-136.8 61.3-136.8 136.8 0 75.5 61.3 136.8 136.8 136.8 75.5 0 136.8-61.3 136.8-136.8z"
-                />
-                <Path
-                  fill="#000"
-                  d="m109.8 252.5c0 7.3 5.9 13.3 13.2 13.3 7.4 0 13.3-6 13.3-13.3v-17h-26.5v17z"
-                />
-                <Path
-                  fill="#000"
-                  d="m206.4 252.5c0 7.3 5.9 13.3 13.2 13.3 7.5 0 13.5-6 13.5-13.3v-17h-26.8z"
-                />
-                <Path
-                  fill="#000"
-                  fill-rule="evenodd"
-                  d="m269.7 126.8c0 4.8-3.9 8.6-8.6 8.6h-11v100.1h-157.3v-143c0-9.4 7.6-17.1 17-17.1h123.2c9.4 0 17.1 7.7 17.1 17.1v25.6h11c4.7 0 8.6 3.9 8.6 8.7zm-129.3-29.5c0 3.6 2.9 6.5 6.5 6.5h48.8c3.6 0 6.5-2.9 6.5-6.5 0-3.6-2.9-6.5-6.5-6.5h-48.8c-3.6 0-6.5 2.9-6.5 6.5zm-8.6 115.1c0-4.9-3.9-8.7-8.8-8.7-4.8 0-8.7 3.8-8.7 8.7 0 4.8 3.9 8.7 8.7 8.7 4.9 0 8.8-3.9 8.8-8.7zm96.6 0c0-4.9-3.9-8.7-8.7-8.7-4.8 0-8.7 3.8-8.7 8.7 0 4.8 3.9 8.7 8.7 8.7 4.8 0 8.7-3.9 8.7-8.7zm8.3-94.3h-130.5v74.7h130.5z"
-                />
-                <Path
-                  fill="#000"
-                  d="m72.3 126.8c0 4.8 3.8 8.6 8.6 8.6h11v-17.3h-11c-4.8 0-8.6 3.9-8.6 8.7z"
-                />
-                <Path fill="#000" d="m181.4 130.1l3.1 3.1-39.5 39.5-3.1-3.1z" />
-                <Path
-                  fill="#000"
-                  d="m192.6 154.3l-26.5 26.5-3.1-3.1 26.5-26.5z"
-                />
-              </G>
-            </G>
+            {/* Circle behind the bus */}
+            <Circle cx={171} cy={200} r={160} fill="rgba(255, 255, 255, 0.6)" />
+            {/* Pointy Thingy */}
+            <Path
+              fill={fillColor}
+              stroke="#fff"
+              strokeWidth={16}
+              d="m341.6 169.4c0 175.4-170.6 338.4-170.6 338.4 0 0-170.6-163-170.6-338.4 0-93.4 76.4-169.2 170.6-169.2 94.2 0 170.6 75.8 170.6 169.2zm-33.8-2.9c0-75.5-61.3-136.8-136.8-136.8-75.5 0-136.8 61.3-136.8 136.8 0 75.5 61.3 136.8 136.8 136.8 75.5 0 136.8-61.3 136.8-136.8z"
+            />
+            {/* Left wheel */}
+            <Path
+              fill={fillColor}
+              d="m109.8 252.5c0 7.3 5.9 13.3 13.2 13.3 7.4 0 13.3-6 13.3-13.3v-17h-26.5v17z"
+            />
+            {/* Right wheel */}
+            <Path
+              fill={fillColor}
+              d="m206.4 252.5c0 7.3 5.9 13.3 13.2 13.3 7.5 0 13.5-6 13.5-13.3v-17h-26.8z"
+            />
+            {/* Vehicle body */}
+            <Path
+              fill={fillColor}
+              fill-rule="evenodd"
+              d="m269.7 126.8c0 4.8-3.9 8.6-8.6 8.6h-11v100.1h-157.3v-143c0-9.4 7.6-17.1 17-17.1h123.2c9.4 0 17.1 7.7 17.1 17.1v25.6h11c4.7 0 8.6 3.9 8.6 8.7zm-129.3-29.5c0 3.6 2.9 6.5 6.5 6.5h48.8c3.6 0 6.5-2.9 6.5-6.5 0-3.6-2.9-6.5-6.5-6.5h-48.8c-3.6 0-6.5 2.9-6.5 6.5zm-8.6 115.1c0-4.9-3.9-8.7-8.8-8.7-4.8 0-8.7 3.8-8.7 8.7 0 4.8 3.9 8.7 8.7 8.7 4.9 0 8.8-3.9 8.8-8.7zm96.6 0c0-4.9-3.9-8.7-8.7-8.7-4.8 0-8.7 3.8-8.7 8.7 0 4.8 3.9 8.7 8.7 8.7 4.8 0 8.7-3.9 8.7-8.7zm8.3-94.3h-130.5v74.7h130.5z"
+            />
+            {/* Rear view mirrors */}
+            <Path
+              fill={fillColor}
+              d="m72.3 126.8c0 4.8 3.8 8.6 8.6 8.6h11v-17.3h-11c-4.8 0-8.6 3.9-8.6 8.7z"
+            />
+            {/* Mirror Streaks */}
+            <Path
+              fill={fillColor}
+              d="m181.4 130.1l3.1 3.1-39.5 39.5-3.1-3.1z"
+            />
+            <Path
+              fill={fillColor}
+              d="m192.6 154.3l-26.5 26.5-3.1-3.1 26.5-26.5z"
+            />
           </Svg>
         </View>
       </CustomAnimatedMarker>
